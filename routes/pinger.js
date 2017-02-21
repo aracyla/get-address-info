9e372ec8b95c927b551b4127bf8b688e40d7aba2
aracytopterm <almeidaleonardor@gmail.com>
2017-02-21 03:34:26 -0300

    non reachable address localization added

--- routes/pinger.js
+++ routes/pinger.js
@@ -7,17 +7,6 @@ const TIMEOUT = 6000;
 const DNS_PUBLIC_TABLE = ["8.8.8.8", "8.8.4.4"];
 
 var isHostAlive = function (host_ip, f_cb){
-    /*
-    tcpp.ping({ address: host_ip, attempts: 1, timeout: 4000 }, function(err, data) {
-        if(data.max)
-            f_cb(true);
-        else
-            f_cb(false);
-    });
-
-    ping.sys.probe(host_ip, function(isAlive){
-        f_cb(isAlive);
-    })*/
     if(DNS_PUBLIC_TABLE.find(function (ip_dns_pub){
         return (ip_dns_pub == host_ip);
     })){
@@ -37,22 +26,14 @@ var isHostAlive = function (host_ip, f_cb){
     });
 
 }
-/* Working on it!
-var reverseIp = function (address, f_cb){
-    dns.reverse(address, function(err, hostnames){
-        if(!err){
-            f_cb(hostnames);
-        }else{
-            console.log(err);
-        }
-    });
-}
-*/
+
 
 var hostnameToIp = function (hostname, f_cb){
     dns.lookup(hostname, function(err, address, family){
         if(!err)
             f_cb(address, family);
+        else
+            f_cb(null, null);
     });
 }
 
@@ -60,6 +41,7 @@ var getIpInformation = function (address){
     var geo = geoip.lookup(address);
     return geo;
 }
+
 
 /*
 address = {ip: value, hostname: value}
@@ -69,42 +51,71 @@ var pinger_exe = function (address, res){
     console.log("=>pinger mainApp:");
     console.log("------Ip: "+address.ip);
     console.log("------Hostname: "+address.hostname);
-//CHECK IF IS ALIVE
+
+//CHECK IF ITS AN IP INFO REQUEST
     if(address.ip){
         address.ip = (stripHTTP(address.ip));
 
         isHostAlive(address.ip, function(isAlive){
-            console.log("------isAlive: "+isAlive);
+            console.log("------isAlive(httpget): "+isAlive);
+            //IF ALIVE MOUNT RESPONSE
             if(isAlive){
-                //if alive mount jsonresponse
                 mount_JSONResponse(address, res);
             }
+            //NOT SURE IF ITS ALIVE
             else{
+                //TRY TO GET GEO INFO
+                JSON_georesponse = mount_JSONGeoResponse(address.ip);
+                //NOT FOUNG GEO INFO - MOUNT NOT FOUND RESPONSE
+                if(!JSON_georesponse){
+                    res.write(JSON.stringify({isAlive: false ,"hostname": address.hostname?address.hostname:false, "ip": address.ip?address.ip:false}));
+                    res.end();
+                }
+                //MOUN ONLY JSON FOR GEO RESPONSE AND UNDEF STATUS
+                else {
+                    var JSON_response = {};
+                    JSON_response["isAlive"] = "undefined";
+                    JSON_response["ip"] = address.ip;
+                    JSON_response["geo"] = JSON_georesponse;
+                    res.write(JSON.stringify(JSON_response));
+                    res.end();
+                }
+            }
+        });
+    }
+    //A HOSTNAME INFO REQUEST
+    else{
+        address.hostname = (stripHTTP(address.hostname));
+        //'CONVERT' HOSTNAME TO IP
+        hostnameToIp(address.hostname, function(ip, family){
+            //IF A VALID IP
+            if(ip) {
+                isHostAlive(ip, function(isAlive){
+                    console.log("isAlive: "+isAlive);
+                    //CHECK IF IP IS ALIVE AND IF TRUE MOUNT JSONRESPONSE
+                    if(isAlive){
+                        mount_JSONResponse(address, res);
+                    }
+                    //IP IS NOT ALIVE - MOUNT NOT FOUND RESPONSE
+                    else{
+                        res.write(JSON.stringify({isAlive: false ,"hostname": address.hostname?address.hostname:false, "ip": address.ip?address.ip:false}));
+                        res.end();
+                    }
+                });
+            }
+            //NON VALID IP
+            else {
+                //MOUNT NOT FOUND RESPONSE
                 res.write(JSON.stringify({isAlive: false ,"hostname": address.hostname?address.hostname:false, "ip": address.ip?address.ip:false}));
                 res.end();
             }
         });
     }
-    else{
-        address.hostname = (stripHTTP(address.hostname));
-
-        hostnameToIp(address.hostname, function(ip, family){
-            isHostAlive(ip, function(isAlive){
-                console.log("isAlive: "+isAlive);
-                if(isAlive){
-                    mount_JSONResponse(address, res);
-                }
-                else{
-                    res.write(JSON.stringify({isAlive: false ,"hostname": address.hostname?address.hostname:false, "ip": address.ip?address.ip:false}));
-                    res.end();
-                }
-            });
-        });
-    }
 }
 
-function mount_JSONResponse(address, res){
 
+
+function mount_JSONResponse(address, res){
     var JSON_response = {};
     JSON_response["isAlive"] = true;
     //res.write(JSON.stringify({isAlive: true}));
@@ -112,48 +123,34 @@ function mount_JSONResponse(address, res){
     if(address.ip){
         console.log("------JSON_ip: "+address.ip);
         JSON_response["ip"] = address.ip;
-
-        var JSON_georesponse = {};
-
-
-        //console.log(JSON_response);
-        var geo = getIpInformation(address.ip);
-
-        JSON_georesponse["country"] = geo.country;
-        JSON_georesponse["state"] = geo.region;
-        JSON_georesponse["city"] = geo.city;
-        JSON_georesponse["lat"] = geo.ll[0];
-        JSON_georesponse["lon"] = geo.ll[1];
-
-        JSON_response["geo"] = JSON_georesponse;
-
+        JSON_response["geo"] = mount_JSONGeoResponse(address.ip);
         res.end(JSON.stringify(JSON_response));
-
-    }else{
-
+    }
+    else{
         hostnameToIp(address.hostname, function(ip, family){
-
             console.log("------JSON_ip: "+ip);
             JSON_response["ip"] = ip;
-
-            var JSON_georesponse = {};
-
-
-            //console.log(JSON_response);
-            var geo = getIpInformation(ip);
-            JSON_georesponse["country"] = geo.country;
-            JSON_georesponse["state"] = geo.region;
-            JSON_georesponse["city"] = geo.city;
-            JSON_georesponse["lat"] = geo.ll[0];
-            JSON_georesponse["lon"] = geo.ll[1];
-
-            JSON_response["geo"] = JSON_georesponse;
-
+            JSON_response["geo"] = mount_JSONGeoResponse(ip);
             res.end(JSON.stringify(JSON_response));
         });
     }
-
 }
+function mount_JSONGeoResponse(ip) {
+    var JSON_georesponse = {};
+
+    //console.log(JSON_response);
+    var geo = getIpInformation(ip);
+    if(!geo.ll[0]) return null;
+    JSON_georesponse["country"] = geo.country;
+    JSON_georesponse["state"] = geo.region;
+    JSON_georesponse["city"] = geo.city;
+    JSON_georesponse["lat"] = geo.ll[0];
+    JSON_georesponse["lon"] = geo.ll[1];
+
+    return JSON_georesponse;
+}
+
+
 
 function stripHTTP(str){
     var http_pattern = /http:\/\//i;
@@ -168,5 +165,4 @@ var Pinger = module.exports = {};
 
 Pinger.mainApp = pinger_exe;
 
-
-//TODO: add an axception for local ips(dns). Use geo ip directly
+//TODO: get informatio from the person that is getting into the site and fill it
